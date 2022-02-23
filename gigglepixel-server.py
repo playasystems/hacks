a263e81f6b5259e7fd200808b6a1cf961772a5a7
Mike Schiraldi <git@schiraldi.org>
2022-02-22 17:31:55 -0800

    Send three-color palettes

--- gigglepixel-server.py
+++ gigglepixel-server.py
@@ -13,23 +13,25 @@ def chr16(w):
 def msg_wrap(msg):
   rv = "GLPX"
   rv += chr(VERSION)
-  rv += chr16(len(msg))
+  rv += chr16(7 + len(msg))
   rv += msg
   return rv
 
 # Construct a GigglePixel message with a single-entry color palette
-def color_msg(r,g,b, priority=1, flags=0, source_id=1, dest_id=1):
+def color_msg(colors, priority=1, flags=0, source_id=1, dest_id=1):
   msg = chr(1)
   msg += chr(priority)
   msg += chr(flags)
   msg += chr16(source_id)
   msg += chr16(dest_id)
-  msg += chr16(1)
-  fraction = 1
-  msg += chr(fraction)
-  msg += chr(r)
-  msg += chr(g)
-  msg += chr(b)
+  msg += chr16(len(colors))
+  for rgb in colors:
+    r,g,b = rgb
+    fraction = 1
+    msg += chr(fraction)
+    msg += chr(r)
+    msg += chr(g)
+    msg += chr(b)
   return msg_wrap(msg)
 
 # Loop endlessly, sending these three RGB values at 1Hz
@@ -37,7 +39,8 @@ colors = [(255,0,0), (0,255,0), (0,0,255)]
 while True:
   rgb = colors.pop(0)
   colors.append(rgb)
-  msg = color_msg(*rgb)
+  msg = color_msg(colors)
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   sock.sendto(msg, (IP, PORT))
+  print "Sent new palette"
   time.sleep(1)
